a5512f250f018627ce08fd801535ba7ab33bbcef
Ha Pham <hnpha5@student.monash.edu>
2020-07-07 09:38:11 +1000

    Add files

---
+++ test/visualgrid-V1.test.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const { Builder, By } = require('selenium-webdriver');
+const { Eyes, VisualGridRunner, Target, RectangleSize, Configuration, BatchInfo, BrowserType, DeviceName, ScreenOrientation} = require('@applitools/eyes-selenium');
+
+describe('Applitools V1 - Ultrafast Grid', function () {
+  let runner, eyes, driver;
+
+  before(async () => {
+
+    // Create a new chrome web driver
+    driver = await new Builder()
+        .forBrowser('chrome')
+        .build();
+
+    // Create a runner with concurrency of 1
+    runner = new VisualGridRunner(1);
+
+    // Create Eyes object with the runner, meaning it'll be a Visual Grid eyes.
+    eyes = new Eyes(runner);
+
+    // Initialize the eyes configuration.
+    let conf = new Configuration()
+
+    // You can get your api key from the Applitools dashboard
+    conf.setApiKey('KIz4L4RkyrfENe1CzA5fGX1ykPAr4w97JabBJWKFuwvw110');
+
+    // create a new batch info instance and set it to the configuration
+    conf.setBatch(new BatchInfo("UFG Hackathon"));
+
+    // Add browsers with different viewports
+    conf.addBrowser(1200, 700, BrowserType.CHROME);
+    conf.addBrowser(1200, 700, BrowserType.FIREFOX);
+    conf.addBrowser(1200, 700, BrowserType.EDGE_CHROMIUM);
+    conf.addBrowser(768, 700, BrowserType.CHROME);
+    conf.addBrowser(768, 700,  BrowserType.FIREFOX);
+    conf.addBrowser(768, 700, BrowserType.EDGE_CHROMIUM);
+
+    // Add mobile emulation devices in Portrait mode
+    conf.addDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.PORTRAIT);
+    conf.addDeviceEmulation(DeviceName.Pixel_2, ScreenOrientation.PORTRAIT);
+
+    // set the configuration to eyes
+    eyes.setConfiguration(conf)
+
+
+  });
+
+  it('ultraFastTest', async () => {
+    // Call Open on eyes to initialize a test session
+    await eyes.open(driver, 'Task 1', 'Cross-Device Elements Test', new RectangleSize(800, 600));
+
+    // Navigate the browser to the "ACME" demo app.
+    // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
+    // but then change the above URL to https://demo.applitools.com/index_v2.html
+    // (for the 2nd run)
+    await driver.get("https://demo.applitools.com/gridHackathonV1.html");
+
+    // Check the app page
+    await eyes.check("Applications UltraFastGrid | Cross Browser Testing Demo App", Target.window().fully());
+
+    // Call Close on eyes to let the server know it should display the results
+    await eyes.closeAsync();
+  });
+
+  after(async () => {
+    // Close the browser.
+    await driver.quit();
+
+    // If the test was aborted before eyes.close was called, ends the test as aborted.
+    await eyes.abortAsync();
+
+    // we pass false to this method to suppress the exception that is thrown if we
+    // find visual differences
+    const allTestResults = await runner.getAllTestResults();
+    console.log(allTestResults);
+  });
+});
+
--- test/visualgrid-V2.test.js
+++ test/visualgrid-V2.test.js
@@ -3,7 +3,7 @@
 const { Builder, By } = require('selenium-webdriver');
 const { Eyes, VisualGridRunner, Target, RectangleSize, Configuration, BatchInfo, BrowserType, DeviceName, ScreenOrientation} = require('@applitools/eyes-selenium');
 
-describe('DemoApp - Ultrafast Grid', function () {
+describe('Applitools V1 - Ultrafast Grid', function () {
   let runner, eyes, driver;
 
   before(async () => {
@@ -23,17 +23,18 @@ describe('DemoApp - Ultrafast Grid', function () {
     let conf = new Configuration()
 
     // You can get your api key from the Applitools dashboard
-    conf.setApiKey('APPLITOOLS_API_KEY');
+    conf.setApiKey('KIz4L4RkyrfENe1CzA5fGX1ykPAr4w97JabBJWKFuwvw110');
 
     // create a new batch info instance and set it to the configuration
-    conf.setBatch(new BatchInfo("Ultrafast Batch"));
+    conf.setBatch(new BatchInfo("UFG Hackathon"));
 
     // Add browsers with different viewports
-    conf.addBrowser(800, 600, BrowserType.CHROME);
-    conf.addBrowser(700, 500, BrowserType.FIREFOX);
-    conf.addBrowser(1600, 1200, BrowserType.IE_11);
-    conf.addBrowser(1024, 768, BrowserType.EDGE_CHROMIUM);
-    conf.addBrowser(800, 600, BrowserType.SAFARI);
+    conf.addBrowser(1200, 700, BrowserType.CHROME);
+    conf.addBrowser(1200, 700, BrowserType.FIREFOX);
+    conf.addBrowser(1200, 700, BrowserType.EDGE_CHROMIUM);
+    conf.addBrowser(768, 700, BrowserType.CHROME);
+    conf.addBrowser(768, 700,  BrowserType.FIREFOX);
+    conf.addBrowser(768, 700, BrowserType.EDGE_CHROMIUM);
 
     // Add mobile emulation devices in Portrait mode
     conf.addDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.PORTRAIT);
@@ -47,23 +48,16 @@ describe('DemoApp - Ultrafast Grid', function () {
 
   it('ultraFastTest', async () => {
     // Call Open on eyes to initialize a test session
-    await eyes.open(driver, 'Demo App', 'Ultrafast grid demo', new RectangleSize(800, 600));
+    await eyes.open(driver, 'Task 1', 'Cross-Device Elements Test', new RectangleSize(800, 600));
 
     // Navigate the browser to the "ACME" demo app.
     // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
     // but then change the above URL to https://demo.applitools.com/index_v2.html
     // (for the 2nd run)
-    await driver.get("https://demo.applitools.com");
-
-    // check the login page with fluent api, see more info here
-    // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
-    await eyes.check("Login Window", Target.window().fully());
-
-    // This will create a test with two test steps.
-    await driver.findElement(By.id("log-in")).click();
+    await driver.get("https://demo.applitools.com/gridHackathonV2.html");
 
     // Check the app page
-    await eyes.check("App Window", Target.window().fully());
+    await eyes.check("Applications UltraFastGrid | Cross Browser Testing Demo App", Target.window().fully());
 
     // Call Close on eyes to let the server know it should display the results
     await eyes.closeAsync();
